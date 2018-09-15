ed514a8a1aeae5d72a850c1da17d35891e23ca71
Jason Tucker <jtgeek@gmail.com>
2018-09-15 00:19:12 -0400

    Getting started

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Campaign from './components/Campaign'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>Track Your Arkham Horror LCG Campaigns</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <Campaign/>
+        </div>
       </div>
     );
   }
---
+++ src/components/Campaign.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Campaign extends Component {
+  render() {
+    return (
+        <div>
+            A campaign
+        </div>
+    );
+  }
+}
+
+export default Campaign;
